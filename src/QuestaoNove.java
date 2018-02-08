adb60755bde44fb2eef9ac0020f335367e104d9c
Samuel Rocha <samuelrodriguessr@hotmail.com>
2018-02-07 23:26:22 -0300

    melhoria

--- src/QuestaoNove.java
+++ src/QuestaoNove.java
@@ -53,45 +53,46 @@ public class QuestaoNove {
         return arvoreA;
     }
 
-}
 
-/**
- * Estrutura de arvore binária
- */
-class BinaryTree {
+    /**
+     * Estrutura de arvore binária
+     */
+    private static class BinaryTree {
 
-    private int valor;
-    private BinaryTree left;
-    private BinaryTree right;
+        private int valor;
+        private BinaryTree left;
+        private BinaryTree right;
 
-    public BinaryTree() {
+        public BinaryTree() {
+        }
+
+        public BinaryTree(int valor) {
+            this.valor = valor;
+        }
+
+        public int getValor() {
+            return valor;
+        }
+
+        public void setValor(int valor) {
+            this.valor = valor;
+        }
+
+        public BinaryTree getLeft() {
+            return left;
+        }
+
+        public void setLeft(BinaryTree left) {
+            this.left = left;
+        }
+
+        public BinaryTree getRight() {
+            return right;
+        }
+
+        public void setRight(BinaryTree right) {
+            this.right = right;
+        }
     }
 
-    public BinaryTree(int valor) {
-        this.valor = valor;
-    }
-
-    public int getValor() {
-        return valor;
-    }
-
-    public void setValor(int valor) {
-        this.valor = valor;
-    }
-
-    public BinaryTree getLeft() {
-        return left;
-    }
-
-    public void setLeft(BinaryTree left) {
-        this.left = left;
-    }
-
-    public BinaryTree getRight() {
-        return right;
-    }
-
-    public void setRight(BinaryTree right) {
-        this.right = right;
-    }
 }
